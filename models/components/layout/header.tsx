b983ada2dfa26b6ca889c174ebd6f2c73f8f4e29
Hyeonglim Doh <hyeonglim.doh@navercorp.com>
2022-05-25 16:33:57 +0900

    add logout btn

--- models/components/layout/header.tsx
+++ models/components/layout/header.tsx
@@ -1,12 +1,22 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { FC } from "react";
 
 const Header: FC = () => {
   const { data: session } = useSession()
 
+  if (!session) 
+    return (
+      <div>
+        {/* LOGO */}
+      </div>
+    )
+
   return (
     <div>
-      
+      <div>
+
+      </div>
+      <button onClick={() => signOut()}>로그아웃</button>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Header from '../models/components/layout/header'
 
 function App({ 
   Component, 
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>  
+    <SessionProvider session={session}>
+      <Header /> 
       <Component {...pageProps} />
     </SessionProvider>
   )
