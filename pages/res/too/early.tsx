58f780268e89c62b4274c2e65d93b1392b2e70a7
Hyeonglim Doh <hyeonglim.doh@navercorp.com>
2022-06-11 19:48:45 +0900

    append meta data

--- pages/res/too/early.tsx
+++ pages/res/too/early.tsx
@@ -7,6 +7,10 @@ const TooEarly: NextPage = () => {
   return (
     <>
       <Head>
+        <meta property="og:url" content="/res/too/early" /> 
+        <meta property="og:type" content="website" /> 
+        <meta property="og:title" content="스터디 결과" />
+        <meta property="og:description" content="아직 결과가 오픈되지 않았습니다" /> 
         <meta property="og:image" content='https://user-images.githubusercontent.com/48513798/173183928-11974793-e983-48ec-b3da-42066deeabbc.png' /> 
       </Head>
       <VStack height='100%' justifyContent='center'>
