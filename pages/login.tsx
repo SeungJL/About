0738baf944e08c510809dba8db6732cfd3ad107d
Hyeonglim Doh <hyeonglim.doh@navercorp.com>
2022-06-20 21:29:32 +0900

    add op meta data to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import { VStack, Heading, Box, Button, HStack, Text, useDisclosure, AlertDialogO
 import { NextPage, GetServerSideProps } from "next"
 import { BuiltInProviderType } from "next-auth/providers"
 import { LiteralUnion, ClientSafeProvider, signIn, getProviders, getSession } from "next-auth/react"
+import Head from "next/head"
 import { useRouter } from "next/router"
 import { useEffect, useRef, useState } from "react"
 import Logo from "../components/logo"
@@ -32,60 +33,69 @@ const Login: NextPage<{
   const kakaoProvider = Object.values(providers).find((p) => p.id == 'kakao')
 
   return (
-    <VStack height='92vh' justifyContent='center'>
-      <VStack marginBottom='20px'>
-        <Logo />
-        <Heading textAlign='center'>VOTE HELPER</Heading>
-      </VStack>
+    <>
+      <Head>
+        <meta property="og:url" content={`${process.env.NEXTAUTH_URL}/login`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="스터디 투표" />
+        <meta property="og:description" content="스터디 투표" />
+        <meta property="og:image" content='/meta_tag_img.png' /> 
+      </Head>
+      <VStack height='92vh' justifyContent='center'>
+        <VStack marginBottom='20px'>
+          <Logo />
+          <Heading textAlign='center'>VOTE HELPER</Heading>
+        </VStack>
 
-      <Box key={kakaoProvider.id}>
-        <Button
-          width='230px'
-          height='60px'
-          backgroundColor='#FEE500'
-          borderRadius='6px'
-          isLoading={loading}
-          onClick={() => customSignin(kakaoProvider)}
+        <Box key={kakaoProvider.id}>
+          <Button
+            width='230px'
+            height='60px'
+            backgroundColor='#FEE500'
+            borderRadius='6px'
+            isLoading={loading}
+            onClick={() => customSignin(kakaoProvider)}
+          >
+            <HStack width='200px' justifyContent='space-between'>
+              <Box display='inline'
+                marginRight='10px'
+              >
+                <svg width="25" height="23" viewBox="0 0 348 317" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M348 135.5C348 210.335 270.098 271 174 271C77.9025 271 0 210.335 0 135.5C0 60.6654 77.9025 0 174 0C270.098 0 348 60.6654 348 135.5Z" fill="#000000"/>
+                  <path d="M91.0331 216.167C91.6506 213.791 93.9017 212.215 96.3456 212.447L193.205 221.656C197.833 222.096 199.408 228.063 195.6 230.729L74.2661 315.688C70.4575 318.355 65.3894 314.834 66.559 310.335L91.0331 216.167Z" fill="#000000"/>
+                </svg>
+              </Box>
+              <Text fontSize='lg' flex='1' display='inline'>카카오 로그인</Text>
+            </HStack>
+          </Button>
+        </Box>
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+          isCentered
+          size='xs'
         >
-          <HStack width='200px' justifyContent='space-between'>
-            <Box display='inline'
-              marginRight='10px'
-            >
-              <svg width="25" height="23" viewBox="0 0 348 317" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M348 135.5C348 210.335 270.098 271 174 271C77.9025 271 0 210.335 0 135.5C0 60.6654 77.9025 0 174 0C270.098 0 348 60.6654 348 135.5Z" fill="#000000"/>
-                <path d="M91.0331 216.167C91.6506 213.791 93.9017 212.215 96.3456 212.447L193.205 221.656C197.833 222.096 199.408 228.063 195.6 230.729L74.2661 315.688C70.4575 318.355 65.3894 314.834 66.559 310.335L91.0331 216.167Z" fill="#000000"/>
-              </svg>
-            </Box>
-            <Text fontSize='lg' flex='1' display='inline'>카카오 로그인</Text>
-          </HStack>
-        </Button>
-      </Box>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-        isCentered
-        size='xs'
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              강제 로그아웃
-            </AlertDialogHeader>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                강제 로그아웃
+              </AlertDialogHeader>
 
-            <AlertDialogBody>
-              <Text>관리자가 당신의 권한을 변경하여 강제 로그아웃되었습니다.</Text>
-            </AlertDialogBody>
+              <AlertDialogBody>
+                <Text>관리자가 당신의 권한을 변경하여 강제 로그아웃되었습니다.</Text>
+              </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button colorScheme='red' onClick={onClose} width='100%'>
-                확인
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </VStack>
+              <AlertDialogFooter>
+                <Button colorScheme='red' onClick={onClose} width='100%'>
+                  확인
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </VStack>
+    </>
   )
 }
 
