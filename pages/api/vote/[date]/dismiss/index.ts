54796ac3c4a5683a4f74f62de29159578ef7af54
승주 <j44s11@naver.com>
2023-02-06 15:48:41 +0900

    취소 버튼

--- pages/api/vote/[date]/dismiss/index.ts
+++ pages/api/vote/[date]/dismiss/index.ts
@@ -1,48 +1,56 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { getToken } from "next-auth/jwt"
-import dbConnect from "../../../../../libs/dbConnect"
-import { strToDate } from "../../../../../libs/utils/dateUtils"
-import { IAbsence, IVote, Vote } from "../../../../../models/vote"
+import { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+import dbConnect from "../../../../../libs/dbConnect";
+import { strToDate } from "../../../../../libs/utils/dateUtils";
+import { IAbsence, IVote, Vote } from "../../../../../models/vote";
 
-const secret = process.env.NEXTAUTH_SECRET
+const secret = process.env.NEXTAUTH_SECRET;
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<IVote>,
+  res: NextApiResponse<IVote>
 ) {
-  const { method } = req
-  const dateStr = req.query.date as string
-  const date = strToDate(dateStr).toDate()
+  const { method } = req;
+  const dateStr = req.query.date as string;
+  const date = strToDate(dateStr).toDate();
 
-  const token = await getToken({ req, secret })
-  const _id = token.id
+  const token = await getToken({ req, secret });
+  const _id = token.id;
 
-  await dbConnect()
+  await dbConnect();
 
-  const vote = await Vote.findOne({ date })
-  if (!vote) return res.status(404).end()
-  
-  const targetAtt = vote.participations.flatMap((p) => p.attendences).find((att) => att.user == _id)
-  
+  const vote = await Vote.findOne({ date });
+  if (!vote) return res.status(404).end();
+
+  const targetAtt = vote.participations
+    .flatMap((p) => p.attendences)
+    .find((att) => att.user == _id);
+
   // 확정하지 않은 경우 불참 처리가 아니라 참여취소처리해야함
   if (!targetAtt || !targetAtt.confirmed) {
-    return res.status(400).end()
+    return res.status(400).end();
   }
 
   switch (method) {
-    case 'PATCH':
+    case "PATCH":
       vote.participations.forEach((participation) => {
-        const isTargetParticipation = !!participation.attendences.find((att) => att.user == _id)
+        const isTargetParticipation = !!participation.attendences.find(
+          (att) => att.user == _id
+        );
         if (isTargetParticipation) {
-          participation.attendences = participation.attendences.filter((att) => att.user != _id)
-          participation.absences = [...participation.absences, { user: _id, noShow: false, message: '' } as IAbsence]
+          participation.attendences = participation.attendences.filter(
+            (att) => att.user != _id
+          );
+          participation.absences = [
+            ...participation.absences,
+            { user: _id, noShow: false, message: "" } as IAbsence,
+          ];
         }
-      })
+      });
 
-      await vote.save()
-      return res.status(204).end()
-    }
-    
+      await vote.save();
+      return res.status(204).end();
+  }
 
-  return res.status(400).end()
+  return res.status(400).end();
 }
