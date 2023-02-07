6c04d4c968e3efe2e1da5bfb9a39a62d8c41dcb6
beming-dev <mingfordev@gmail.com>
2023-02-07 22:49:59 +0900

    Update book fetch

--- pages/api/book/index.ts
+++ pages/api/book/index.ts
@@ -8,12 +8,15 @@ export default async function handler(
   process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
   switch (method) {
     case "GET":
-      console.log(1);
-      const res = await fetch(
+      const response = await fetch(
         `https://data4library.kr/api/loanItemSrch?authKey=0a18d855cdbe794e1225816ee8f65f7e71c94ffcb19cba97e9c90e1199e078ff&startDt=2022-01-01&endDt=2022-03-31&gender=1&age=20`
       );
-      const data = await res.text();
-      console.log(data);
+      if (response.status === 200) {
+        const data = await response.text();
+        res.status(200).send(data);
+      } else {
+        throw new Error("unable to get api");
+      }
   }
 
   return res.status(400).end();
