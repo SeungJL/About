9e8b1f8e6ce9dfb9c17b694748c83a846be17ba2
승주 <j44s11@naver.com>
2023-02-10 00:00:18 +0900

    유저 가입 폼

--- models/UserInfoForm.tsx
+++ models/UserInfoForm.tsx
@@ -1,19 +1,18 @@
 import dayjs from "dayjs";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { now } from "../libs/utils/dateUtils";
 import { BaseModal } from "../styles/LayoutStyles";
 
 const ModalLayout = styled(BaseModal)`
-  width: 200px;
-  height: 240px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  width: 240px;
+  height: 270px;
 `;
 
 const Header = styled.header`
   height: 28px;
   font-size: 1.2em;
+  margin-bottom: 10px;
 `;
 const Footer = styled.footer`
   height: 28px;
@@ -25,27 +24,52 @@ const Footer = styled.footer`
     border-radius: 10px;
   }
 `;
-const InputItem = styled.div`
-  height: 30px;
+
+const UserForm = styled.form`
   display: flex;
   flex-direction: column;
-  > div {
-    display: flex;
-    > span {
-      width: 60px;
-    }
-    > input {
-      width: 100px;
-      background-color: pink;
-    }
-  }
+`;
+
+const ErrorMessage = styled.div`
+  font-size: 0.8em;
+  height: 16px;
+  color: brown;
+`;
+
+const InputItem = styled.div`
+  height: 20px;
+  display: flex;
+  margin: 5px 0;
+
   > span {
-    font-size: 0.7em;
+    font-size: 0.9em;
+    width: 33%;
+  }
+  > input {
+    width: 67%;
+    background-color: rgb(0, 0, 0, 0.1);
+  }
+`;
+const SubmitBtn = styled.div`
+  display: flex;
+
+  > input {
+    margin-right: 18px;
+  }
+  > button {
+    margin-right: 3px;
+    background-color: brown;
+    color: white;
+    width: 50px;
+    border-radius: 15px;
   }
 `;
 
 interface IUserInfoForm {
-  JoinDate: string;
+  joinDate: string;
+  name: string;
+  mbti?: string;
+  birth: string;
 }
 
 function UserInfoForm() {
@@ -55,7 +79,12 @@ function UserInfoForm() {
     watch,
     formState: { errors },
   } = useForm<IUserInfoForm>({
-    defaultValues: {},
+    defaultValues: {
+      joinDate: `${now().format("YYYY-MM-DD")}`,
+      name: "",
+      mbti: "",
+      birth: "",
+    },
   });
   const onValid = (data: any) => {
     console.log(10, data);
@@ -63,68 +92,67 @@ function UserInfoForm() {
 
   return (
     <>
-      <ModalLayout as="form" onSubmit={handleSubmit(onValid)}>
+      <ModalLayout>
         <Header>사용자 정보</Header>
-        <InputItem>
-          <div>
+        <UserForm onSubmit={handleSubmit(onValid)}>
+          <InputItem>
             <span>가입일: </span>
             <input
-              {...register("JoinDate", {
+              {...register("joinDate", {
                 required: "필수입력",
                 pattern: {
                   value: /^\d{4}-\d{2}-\d{2}$/,
                   message: "한글로 입력헤주세요.",
                 },
               })}
+              placeholder="YYYY-MM-DD"
             />
-          </div>
-          <span>{errors?.JoinDate?.message}</span>
-        </InputItem>
-        <InputItem>
-          <div>
+          </InputItem>
+          <ErrorMessage>{errors?.joinDate?.message}</ErrorMessage>
+          <InputItem>
             <span>이름: </span>
             <input
-              {...register("JoinDate", {
+              {...register("name", {
                 required: "필수입력",
-                pattern: {
-                  value: /^\d{4}-\d{2}-\d{2}$/,
-                  message: "한글로 입력헤주세요.",
+                maxLength: {
+                  value: 3,
+                  message: "세글자로 입력해주세요.",
+                },
+                minLength: {
+                  value: 3,
+                  message: "세글자로 입력해주세요.",
                 },
               })}
             />
-          </div>
-          <span>{errors?.JoinDate?.message}</span>
-        </InputItem>
-        <InputItem>
-          <div>
+          </InputItem>
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+          <InputItem>
             <span>생년월일: </span>
             <input
-              {...register("JoinDate", {
+              {...register("birth", {
                 required: "필수입력",
                 pattern: {
-                  value: /^\d{4}-\d{2}-\d{2}$/,
-                  message: "한글로 입력헤주세요.",
+                  value: /^\d{6}$/,
+                  message: "주민번호 앞자리",
                 },
               })}
+              placeholder="Ex) 970816"
             />
-          </div>
-          <span>{errors?.JoinDate?.message}</span>
-        </InputItem>
-        <InputItem>
-          <div>
-            <span>MBTI: </span>
-            <input
-              {...register("JoinDate", {
-                required: "필수입력",
-                pattern: {
-                  value: /^\d{4}-\d{2}-\d{2}$/,
-                  message: "한글로 입력헤주세요.",
-                },
-              })}
-            />
-          </div>
-          <span>{errors?.JoinDate?.message}</span>
-        </InputItem>
+          </InputItem>
+          <ErrorMessage>{errors?.birth?.message}</ErrorMessage>
+          <InputItem>
+            <span>MBTI(선택): </span>
+            <input {...register("mbti")} placeholder="Ex) ENFP" />
+          </InputItem>
+          <ErrorMessage>{errors?.mbti?.message}</ErrorMessage>
+          <SubmitBtn>
+            <button>약관</button>
+            <span>동의</span>
+            <input type="checkbox" />
+            <button>취소</button>
+            <button>제출</button>
+          </SubmitBtn>
+        </UserForm>
       </ModalLayout>
     </>
   );
