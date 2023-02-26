0a6dbb65ba6ad5d765e113d4452db970f5609ce7
SeungJu <84257439+banana970@users.noreply.github.com>
2023-02-26 16:14:20 +0900

    Vote id error fix

--- libs/utils/voteUtils.ts
+++ libs/utils/voteUtils.ts
@@ -5,7 +5,19 @@ export const getDefaultVoteDate = (isUserAttend: boolean) => {
   const current = now();
   const today = getToday();
   const startHour = today.hour(VOTE_START_HOUR);
-  if (current < startHour) return today;
-  if (isUserAttend && current < now().hour(18)) return today;
+  console.log("current :", current);
+  console.log("today :", today);
+  console.log(3);
+  console.log(startHour);
+  console.log(isUserAttend);
+  if (isUserAttend && current < now().hour(18)) {
+    console.log(4);
+    return today;
+  }
+  if (current < startHour) {
+    console.log(2);
+    return today;
+  }
+
   return today.add(1, "day");
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { RecoilRoot } from "recoil";
 import "nprogress/nprogress.css";
 import "../styles/variable.css";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 const NAVER_CLIENT_ID = process.env.NAVER_CLIENT_ID;
 
@@ -28,6 +29,12 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
       <SessionProvider session={session}>
         <QueryClientProvider client={queryClient}>
           <RecoilRoot>
