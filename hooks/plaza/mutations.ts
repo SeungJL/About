d6f5fa65a870849b5eefe520e549c1fe92ad6755
beming-dev <mingfordev@gmail.com>
2023-03-11 20:09:36 +0900

    Add plaza collection

---
+++ hooks/plaza/mutations.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosError } from "axios";
+import { useMutation, UseMutationOptions } from "react-query";
+import { IPlazaData } from "../../models/plaza";
+
+export const usePlazaMutation = (
+  options?: Omit<
+    UseMutationOptions<void, AxiosError, IPlazaData>,
+    "mutationKey" | "mutationFn"
+  >
+) =>
+  useMutation<void, AxiosError, IPlazaData>(async (suggestionForm) => {
+    await axios.post(`/api/plaza`, { suggestionForm });
+  }, options);
