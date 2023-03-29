4825f5262af64de29aaa71c61b1056f9dcf1fa21
beming-dev <mingfordev@gmail.com>
2023-03-29 11:56:54 +0900

    Add vote start time get

--- pages/api/vote/[date]/start.ts
+++ pages/api/vote/[date]/start.ts
@@ -1,18 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../../libs/dbConnect";
-import { getToday } from "../../../../libs/utils/dateUtils";
+import { getToday, strToDate } from "../../../../libs/utils/dateUtils";
 import { findOneVote } from "../../../../services/voteService";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  const dateStr = req.query.date as string;
+  const dayjsDate = strToDate(dateStr);
+  const date = dayjsDate.toDate();
+
   const { method } = req;
   await dbConnect();
-  console.log(getToday().toDate());
-  const vote = findOneVote(getToday().toDate());
+  const vote = await findOneVote(date);
+
   switch (method) {
     case "GET":
+      const result = [];
+      vote.participations.map((participation) => {
+        if (participation.status === "open") {
+          const openInfo = {
+            place_id: participation.place._id,
+            startTime: participation.startTime,
+          };
+          result.push(openInfo);
+        }
+      });
+      res.status(200).json(result);
       break;
   }
 }
