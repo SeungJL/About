072501f9890fd6aad1b182c29c5d359d5635be56
beming-dev <mingfordev@gmail.com>
2023-04-05 21:57:48 +0900

    Add admin button

--- pages/admin/control.tsx
+++ pages/admin/control.tsx
@@ -1,14 +1,39 @@
 import { Button, Flex } from "@chakra-ui/react";
+import axios from "axios";
 
 export default function Admin() {
-  const onResetScore = () => {};
-  const onResetPoint = () => {};
-  const onDeleteVote = () => {};
+  const secretKey = process.env.NEXTAUTH_SECRET;
+
+  const onResetScore = () => {
+    if (confirm("정말 초기화 하시겠습니까?")) {
+      axios.delete("/api/admin/user/score");
+    }
+  };
+  const onResetPoint = () => {
+    if (confirm("정말 초기화 하시겠습니까?")) {
+      axios.delete("/api/admin/user/point");
+    }
+  };
+  const onDeleteVote = () => {
+    if (confirm("정말 초기화 하시겠습니까?")) {
+      axios.delete("/api/admin/vote", {
+        headers: {
+          secret: secretKey,
+        },
+      });
+    }
+  };
   return (
     <Flex flexDir="column">
-      <Button>score초기화</Button>
-      <Button>point초기화</Button>
-      <Button>현재날짜 + 3일 이후 투표 모두 삭제</Button>
+      <Button onClick={onResetScore} m="5px 0" disabled>
+        score초기화
+      </Button>
+      <Button onClick={onResetPoint} m="5px 0" disabled>
+        point초기화
+      </Button>
+      <Button onClick={onDeleteVote} m="5px 0">
+        현재날짜 + 3일 이후 투표 모두 삭제
+      </Button>
       <Button></Button>
     </Flex>
   );
