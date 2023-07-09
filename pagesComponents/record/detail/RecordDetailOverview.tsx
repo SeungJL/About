207da168d022ec30b80f046808605d44aaac9cdc
SeungJu <84257439+banana970@users.noreply.github.com>
2023-07-10 00:10:52 +0900

    2

--- pagesComponents/record/detail/RecordDetailOverview.tsx
+++ pagesComponents/record/detail/RecordDetailOverview.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import styled from "styled-components";
-
 interface IRecordDetailOverview {
   children: React.ReactNode;
 }
 
 function RecordDetailOverview({ children }: IRecordDetailOverview) {
-  return <Layout>{children}</Layout>;
+  const [isFirst, setIsFirst] = useState();
+
+  return (
+    <Layout>
+      {children}
+      <Container>
+        <Title></Title>
+      </Container>
+    </Layout>
+  );
 }
 
 const Layout = styled.div`
   height: 160px;
+  background-color: var(--color-mint);
 `;
+
+const Container = styled.div``;
+
+const Title = styled.span``;
+
+const Value = styled.span``;
+
+const changeBtn = styled.button``;
 
 export default RecordDetailOverview;
