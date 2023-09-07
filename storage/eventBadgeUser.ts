8143366903f390fc9e7cffd10bab5ac90550b849
SeungJu <84257439+banana970@users.noreply.github.com>
2023-09-07 17:51:47 +0900

    13

--- storage/eventBadgeUser.ts
+++ storage/eventBadgeUser.ts
@@ -1,2 +1,2 @@
-export const EVENT_BADGE_라벤더 = ["2941876134"];
-export const EVENT_BADGE_딸기스무디 = [];
+export const EVENT_BADGE_라벤더 = ["2941876134", "2948913118", "2867069006"];
+export const EVENT_BADGE_딸기스무디 = ["2948913118"];
