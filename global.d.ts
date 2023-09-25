c0075a7df587a4e9738e0f1b90d779b630a27128
SeungJu <84257439+banana970@users.noreply.github.com>
2023-09-25 15:33:49 +0900

    123

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,15 @@
-import { Mongoose } from 'mongoose'
+import { Mongoose } from "mongoose";
+import "react-fontawesome";
 
 export declare global {
   var mongoose: {
-    conn?: Mongoose,
-    promise?: Promise<Mongoose>,
-  }
+    conn?: Mongoose;
+    promise?: Promise<Mongoose>;
+  };
 
-  var _mongoClientPromise: Promise<MongoClient>,
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+declare module "react-fontawesome" {
+  export type FlipProp = "horizontal" | "vertical" | "both" | boolean;
 }
