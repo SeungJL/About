b0f874c32415ebc1673f12aa8e42997c85f6a691
SeungJu <84257439+banana970@users.noreply.github.com>
2023-12-26 00:34:31 +0900

    .

--- constants/contents/GroupStudyContents.tsx
+++ constants/contents/GroupStudyContents.tsx
@@ -27,7 +27,7 @@ export const GROUP_STUDY_CATEGORY_ARR: GroupStudyCategory[] = [
 ];
 
 export const GROUP_STUDY_SUB_CATEGORY = {
-  어학: ["토익", "오픽", "회화"],
+  어학: ["토익", "오픽", "토플", "회화", "일본어"],
   자격증: ["컴활", "한국사", "정보처리기사"],
   프로그래밍: ["코딩테스트", "프로젝트", "언어 공부"],
   취업준비: ["공기업 면접", "사기업 면접", "인적성(NCS)"],
