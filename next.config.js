bced508e96d290a019dd7473fb904393cdcc246b
seungju <j44s11@naver.com>
2024-02-29 01:18:09 +0900

    .

--- next.config.js
+++ next.config.js
@@ -31,6 +31,10 @@ const nextConfig = {
         protocol: "http",
         hostname: "k.kakaocdn.net",
       },
+      {
+        protocol: "http",
+        hostname: "t1.kakaocdn.net",
+      },
     ],
   },
 
