f96e660ca6df461e33f82ff27b82f91eff5f504c
seungju <j44s11@naver.com>
2024-03-11 21:58:36 +0900

    .

--- components/base/Layout.tsx
+++ components/base/Layout.tsx
@@ -35,17 +35,17 @@ function Layout({ children }: ILayout) {
 
   useEffect(() => {
     if (PUBLIC_SEGMENT.includes(segment)) return;
-
-    if (session !== undefined && !session?.user?.location) {
+    if (session === undefined) return;
+    const role = session?.user.role;
+    if (role === "newUser") router.push("/register/location");
+    if (role === "waiting") router.push("/login?status=waiting");
+    if (!session?.user?.location) {
       toast(
         "warning",
         "업데이트가 필요합니다. 다시 로그인 해주세요! 반복되는 경우 관리자에게 문의 부탁드립니다!!"
       );
       signOut({ callbackUrl: `/login/?status=logout` });
     }
-    const role = session?.user.role;
-    if (role === "newUser") router.push("/register/location");
-    if (role === "waiting") router.push("/login?status=waiting");
   }, [session]);
 
   return (
