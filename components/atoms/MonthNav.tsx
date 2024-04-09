dde1d8c51ea10eccd017d24f5fb7dec2f5040447
seungju <j44s11@naver.com>
2024-04-09 19:28:35 +0900

    .

--- components/atoms/MonthNav.tsx
+++ components/atoms/MonthNav.tsx
@@ -2,7 +2,7 @@ import { faCaretLeft, faCaretRight } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Dayjs } from "dayjs";
 import styled from "styled-components";
-import { DispatchType } from "../../types/components/modalTypes";
+import { DispatchType } from "../../types/hooks/reactTypes";
 
 interface IMonthNav {
   month: number;
