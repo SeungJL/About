036705e46e9fe7bd966cbbe42b1abc6fc481446e
seungju <j44s11@naver.com>
2024-04-10 16:07:08 +0900

    (feat: storybook) and has eslint error

---
+++ components/atoms/buttons/BasicButton.tsx
@@ -0,0 +1,55 @@
+// import './button.css';
+
+import { Button as ChakraButton } from "@chakra-ui/react";
+export interface BasicButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const BasicButton = ({
+  primary = true,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: BasicButtonProps) => {
+
+    const secondaryOptions = {
+        color: "mint",
+        backgroundColor: "white",
+        border:"1.2px solid #00c2b3"
+    }
+ 
+  return (
+    <ChakraButton
+      type="button"
+          colorScheme={primary ? "mintTheme" : null}
+          {...(!primary ? secondaryOptions : {})}
+      {...props}
+    >
+      {label}
+   
+    </ChakraButton>
+  );
+};
---
+++ stories/atoms/buttons/BasicButton.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { BasicButton } from '../../../components/atoms/buttons/BasicButton';
+
+
+const meta = {
+  title: 'ATOMS/Button/BasicButton',
+  component: BasicButton,
+  parameters: {
+   
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  
+  args: { onClick: fn() },
+} satisfies Meta<typeof BasicButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    primary:false,
+    label: 'Button',
+  },
+};
+
