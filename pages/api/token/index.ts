e5dc1dd53335ebbe8456cf326391030847ce1870
seungju <j44s11@naver.com>
2024-04-16 19:08:27 +0900

    fix: deleted api auth token

---
+++ pages/api/token/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  const secret = process.env.NEXTAUTH_SECRET;
+
+  switch (method) {
+    case "GET":
+      const jwt = await getToken({ req, secret, raw: true });
+      res.status(200).json(jwt);
+      break;
+  }
+}
